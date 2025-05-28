b7c28b07abab249075148d6de897054ff668c7e0
sourabhupadhayay <131138900+sourabhupadhayay@users.noreply.github.com>
2025-05-28 11:57:57 +0530

    Update certbot-setup.sh

--- certbot-setup.sh
+++ certbot-setup.sh
@@ -7,9 +7,14 @@ EMAIL="sourbhupadhayay@gmail.com"
 sudo apt update
 sudo apt install -y certbot
 
+# Stop any service using port 80 to let certbot bind
+sudo fuser -k 80/tcp || true
+
 sudo certbot certonly --standalone -d $DOMAIN --agree-tos --email $EMAIL --non-interactive
 '''
+    sh 'sudo apt update && sudo apt install -y dos2unix'
     sh 'chmod +x certbot-setup.sh'
+    sh 'dos2unix certbot-setup.sh'
     sh './certbot-setup.sh'
   }
 }
