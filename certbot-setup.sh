47624b4bed02878463a1ab004070b7c2ec895440
sourabhupadhayay <131138900+sourabhupadhayay@users.noreply.github.com>
2025-05-28 12:38:36 +0530

    Update certbot-setup.sh

--- certbot-setup.sh
+++ certbot-setup.sh
@@ -2,9 +2,10 @@
 DOMAIN="app.4xexch.com"
 EMAIL="sourbhupadhayay@gmail.com"
 
-# Stop anything using port 80
-fuser -k 80/tcp || true
+# Stop any container that might be using port 80 (like nginx)
+docker ps --filter "publish=80" --format "{{.ID}}" | xargs -r docker stop
 
+# Run certbot in standalone mode via Docker
 docker run --rm -p 80:80 \
   -v /etc/letsencrypt:/etc/letsencrypt \
   -v /var/lib/letsencrypt:/var/lib/letsencrypt \
