0916b0de72132f0aa4b6e0896efccbb386ab08e9
sourabhupadhayay <131138900+sourabhupadhayay@users.noreply.github.com>
2025-05-28 12:18:08 +0530

    Update certbot-setup.sh

--- certbot-setup.sh
+++ certbot-setup.sh
@@ -1,20 +1,13 @@
-stage('Run Certbot') {
-  steps {
-    writeFile file: 'certbot-setup.sh', text: '''#!/bin/bash
+#!/bin/bash
 DOMAIN="app.4xexch.com"
 EMAIL="sourbhupadhayay@gmail.com"
 
-sudo apt update
-sudo apt install -y certbot
+apt update
+apt install -y certbot
 
-# Stop any service using port 80 to avoid conflict
-sudo fuser -k 80/tcp || true
-
-sudo certbot certonly --standalone -d $DOMAIN --agree-tos --email $EMAIL --non-interactive
-'''
-    sh 'chmod +x certbot-setup.sh'
-    sh "sed -i 's/\r//' certbot-setup.sh"
-    sh './certbot-setup.sh'
-  }
-}
-
+certbot certonly --webroot \
+  -w /usr/share/nginx/html \
+  -d $DOMAIN \
+  --agree-tos \
+  --email $EMAIL \
+  --non-interactive
