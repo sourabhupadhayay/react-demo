d7bab75c0ac2bf6342acdb201592d868b3608ca9
sourabhupadhayay <131138900+sourabhupadhayay@users.noreply.github.com>
2025-05-28 12:33:21 +0530

    Update certbot-setup.sh

--- certbot-setup.sh
+++ certbot-setup.sh
@@ -2,12 +2,11 @@
 DOMAIN="app.4xexch.com"
 EMAIL="sourbhupadhayay@gmail.com"
 
-apt update
-apt install -y certbot
+# Stop anything using port 80
+fuser -k 80/tcp || true
 
-certbot certonly --webroot \
-  -w /usr/share/nginx/html \
-  -d $DOMAIN \
-  --agree-tos \
-  --email $EMAIL \
-  --non-interactive
+docker run --rm -p 80:80 \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  -v /var/lib/letsencrypt:/var/lib/letsencrypt \
+  certbot/certbot certonly --standalone \
+  -d "$DOMAIN" --agree-tos --email "$EMAIL" --non-interactive
